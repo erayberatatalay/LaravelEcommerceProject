184b7841f9da0e86df54bf054a6e8c8cf0877966
Eray Berat Atalay <erayberat37@gmail.com>
2023-01-16 21:15:26 +0300

    Route Yapısı

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/merhaba', function () {
+    return 'welcome';
+});
+
+Route::get('/api/v1/merhaba', function () {
+    return ['mesaj' => 'welcome'];
+});
+
+Route::get('/urun/{urunadi}/{id?}', function ($urunadi, $id = 0) {
+    return "Ürün Adı: $urunadi $id";
+})->name('urun_detay');
+
+Route::get('/kampanya', function () {
+    return redirect()->route('urun_detay', ['urunadi' => 'elma', 'id' => 5]);
+});
