cc05945f8a34bbdbe2f2b74648248f3f7c15e3ea
Eray Berat Atalay <erayberat37@gmail.com>
2023-01-16 21:28:15 +0300

    Controller Yapısı

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,6 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+/*Route::get('/', function () {
+    return view('anasayfa');
 });
 
 Route::get('/merhaba', function () {
@@ -29,4 +17,5 @@ Route::get('/urun/{urunadi}/{id?}', function ($urunadi, $id = 0) {
 
 Route::get('/kampanya', function () {
     return redirect()->route('urun_detay', ['urunadi' => 'elma', 'id' => 5]);
-});
+});*/
+Route::get('/', 'AnasayfaController@index');
